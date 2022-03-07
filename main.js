332a39c1f2f258d72a65ab2702a412da17c58999
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-03-07 17:41:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function clearCanvas() {
 function preload(){
     classifier=ml5.imageClassifier('DoodleNet');
 }
+
+function draw(){
+strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+}
+    
+    function classifyCanvas(){
+    classifier.classify(canvas, gotResult)
+    }
+
+function gotResult(error, results) { if (error) { console.error(error); }
+console.log(results); drawn_sketch = results[0].label; 
+document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch; document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterance(result[0].label);
+                                    syth.speak(utterThis);
+                                   }
